50c58a9bb61dc4650e92b76d41dbb646cb763613
Chaitanya Gupta <chaitanya.gupta@veersatech.com>
2024-10-15 19:18:30 +0530

    using cluserip in microservices + deployment and services config

--- comments/index.js
+++ comments/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
-const morgan = require('morgan')
+const morgan = require("morgan");
 const axios = require("axios");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 const commentsByPostId = {};
 
@@ -23,48 +23,48 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
 
   const comments = commentsByPostId[postId] || [];
-  comments.push({ id: commentId, content, status: 'pending' });
+  comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[postId] = comments;
 
-  await axios.post('http://localhost:4005/events', {
-    type: 'CommentCreated',
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "CommentCreated",
     data: {
       id: commentId,
       content,
       postId,
-      status: 'pending'
-    }
-  })
+      status: "pending",
+    },
+  });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', async (req, res) => {
-  console.log('Event Received', req.body.type)
+app.post("/events", async (req, res) => {
+  console.log("Event Received", req.body.type);
 
   const { type, data } = req.body;
 
-  if (type === 'CommentModerated') {
+  if (type === "CommentModerated") {
     const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
-    const comment = comments.find(comment => {
+    const comment = comments.find((comment) => {
       return comment.id === id;
     });
     comment.status = status;
 
-    await axios.post('http://localhost:4005/events', {
-      type: 'CommentUpdated',
+    await axios.post("http://event-bus-srv:4005/events", {
+      type: "CommentUpdated",
       data: {
         id,
         status,
         postId,
-        content
-      }
+        content,
+      },
     });
   }
 
-  res.send({})
-})
+  res.send({});
+});
 
 app.listen(4001, () => {
   console.log("Listening comments service on 4001");
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const axios = require('axios')
-const cors = require('cors')
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const events = []
+const events = [];
 
-app.post('/events', (req, res) => {
-  const event = req.body
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-  events.push(event)
+  events.push(event);
 
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
-  axios.post('http://localhost:4003/events', event)
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  axios.post("http://comments-clusterip-srv:4001/events", event);
+  axios.post("http://query-clusterip-srv:4002/events", event);
+  axios.post("http://moderation-clusterip-srv:4003/events", event);
 
-  res.send({status: 'OK'})
+  res.send({ status: "OK" });
 });
 
-app.get('/events', (req, res) => {
-  res.send(events)
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log('Listening event bus on 4005')
-})
+  console.log("Listening event bus on 4005");
+});
